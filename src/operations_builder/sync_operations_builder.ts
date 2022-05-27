12841fe538a6c5ad3f35f20e698eb929f5421135
tokebe <43009413+tokebe@users.noreply.github.com>
2022-05-27 12:43:29 -0400

    fix: apply api list to non-reasoner ops builder

--- src/operations_builder/sync_operations_builder.ts
+++ src/operations_builder/sync_operations_builder.ts
@@ -1,6 +1,8 @@
 import BaseOperationsBuilder from "./base_operations_builder";
 import { syncLoaderFactory } from "../load/sync_loader_factory";
 import { BuilderOptions } from "../types";
+import Debug from "debug";
+const debug = Debug("bte:smartapi-kg:SyncOperationsBuilder");
 
 export default class SyncOperationsBuilder extends BaseOperationsBuilder {
   private _file_path: string;
@@ -17,8 +19,19 @@ export default class SyncOperationsBuilder extends BaseOperationsBuilder {
       this._options.tag,
       this._options.component,
       this._options.apiList,
-      this._file_path
+      this._file_path,
     );
-    return this.loadOpsFromSpecs(specs);
+    const ops = this.loadOpsFromSpecs(specs);
+    if (!(typeof this._options.apiList === "undefined")) {
+      return ops.filter(op => {
+        let api = this._options.apiList.find(api => api.id === op.association.smartapi.id);
+        if (api && api.name !== op.association.api_name) {
+          debug(
+            `Expected to get '${api.name}' with smartapi-id:${api.id} but instead got '${op.association.api_name}'`,
+          );
+        }
+        return api;
+      });
+    }
   }
 }
