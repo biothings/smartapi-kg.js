87b673a22e358ff1ca31ac4a40bc59fd02e60cf5
tokebe <43009413+tokebe@users.noreply.github.com>
2022-08-03 15:27:05 -0400

    fix:smartapi size query

--- src/config.ts
+++ src/config.ts
@@ -16,16 +16,16 @@ export const FILTER_FIELDS: FIELDS_FOR_FILTER[] = [
 ];
 
 export const SMARTAPI_URL =
-  "https://smart-api.info/api/query?q=tags.name:translator&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta";
+  "https://smart-api.info/api/query?q=tags.name:translator&size=1000&fields=paths,servers,tags,components.x-bte*,info,_meta";
 
 export const SINGLE_API_SMARTAPI_QUERY_TEMPLATE =
   "https://smart-api.info/api/metadata/{smartapi_id}";
 
 export const TEAM_SMARTAPI_QUERY_TEMPLATE =
-  'https://smart-api.info/api/query?q=info.x-translator.team:"{team_name}"&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta';
+  'https://smart-api.info/api/query?q=info.x-translator.team:"{team_name}"&size=1000&fields=paths,servers,tags,components.x-bte*,info,_meta';
 
 export const COMPONENT_SMARTAPI_QUERY_TEMPLATE =
-  'https://smart-api.info/api/query?q=info.x-translator.component:"{component_name}"&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta';
+  'https://smart-api.info/api/query?q=info.x-translator.component:"{component_name}"&size=1000&fields=paths,servers,tags,components.x-bte*,info,_meta';
 
 export const TAG_SMARTAPI_QUERY_TEMPLATE =
-  "https://smart-api.info/api/query?q=tags.name:{tag_name}&size=150&fields=paths,servers,tags,components.x-bte*,info,_meta";
+  "https://smart-api.info/api/query?q=tags.name:{tag_name}&size=1000&fields=paths,servers,tags,components.x-bte*,info,_meta";
