bc4e020125da57935e83301c46dabdcebc24530a
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2024-11-05 17:34:13 -0500

    fix: make sync kg construction support async file locks

--- src/operations_builder/sync_operations_builder.ts
+++ src/operations_builder/sync_operations_builder.ts
@@ -1,9 +1,6 @@
 import BaseOperationsBuilder from "./base_operations_builder";
 import { syncLoaderFactory } from "../load/sync_loader_factory";
 import { BuilderOptions } from "../types";
-import { lockWithActionSync } from "@biothings-explorer/utils";
-import { SmartAPISpec } from "../parser/types";
-const debug = require("debug")("bte:smartapi-kg:SyncOperationsBuilder");
 
 export default class SyncOperationsBuilder extends BaseOperationsBuilder {
   private _file_path: string;
@@ -14,18 +11,15 @@ export default class SyncOperationsBuilder extends BaseOperationsBuilder {
   }
 
   build() {
-    let specs: SmartAPISpec[] = lockWithActionSync(this._file_path, () => {
-      return syncLoaderFactory(
-        this._options.smartAPIID,
-        this._options.teamName,
-        this._options.tag,
-        this._options.component,
-        this._options.apiList,
-        this._file_path,
-        this._options.smartapiSpecs
-      );
-    }, debug);
-
+    const specs = syncLoaderFactory(
+      this._options.smartAPIID,
+      this._options.teamName,
+      this._options.tag,
+      this._options.component,
+      this._options.apiList,
+      this._file_path,
+      this._options.smartapiSpecs
+    );
     return this.loadOpsFromSpecs(specs);
   }
 }
